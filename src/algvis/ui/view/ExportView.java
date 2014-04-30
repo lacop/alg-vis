300f8e220ce2e80c6c7fb82a313aabcdea93df52
Laco Pápay <laco.papay@gmail.com>
2014-04-30 14:22:19 +0200

    Proper bounding box, font fix

--- src/algvis/ui/view/ExportView.java
+++ src/algvis/ui/view/ExportView.java
@@ -1,32 +1,39 @@
 package algvis.ui.view;
 
+import algvis.ds.dictionaries.bst.BST;
 import algvis.ui.VisPanel;
 
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import de.erichseifert.vectorgraphics2d.SVGGraphics2D;
+import de.erichseifert.vectorgraphics2d.PDFGraphics2D;
+import de.erichseifert.vectorgraphics2d.VectorGraphics2D;
 
 public class ExportView extends View {
-    SVGGraphics2D g;
+    PDFGraphics2D g;
 
     public ExportView(VisPanel panel) {
         super(panel);
 
         Rectangle2D bb = panel.scene.getBoundingBox();
 
-        g = new SVGGraphics2D(bb.getMinX(), bb.getMinY(), bb.getWidth(), bb.getHeight());
+        g = new PDFGraphics2D(bb.getMinX(), bb.getMinY(), bb.getMaxX(), bb.getMaxY());
+
         setGraphics(g, (int) bb.getWidth(), (int) bb.getHeight());
+
+        g.translate(0, -bb.getMinY());
+        g.setFontRendering(VectorGraphics2D.FontRendering.VECTORS);
     }
 
     public void save() {
         try {
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss") ;
             String filename = dateFormat.format(new Date());
-            FileOutputStream file = new FileOutputStream(filename + ".svg");
+            FileOutputStream file = new FileOutputStream(filename + ".pdf");
             file.write(g.getBytes());
             file.close();
         } catch (IOException e) {
